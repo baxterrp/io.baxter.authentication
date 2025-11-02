01058d87c56cd4fe28582528bb093203441e2622
robert baxter <baxterrp@gmail.com>
2025-11-02 17:26:38 -0500

    cleanup base test class

--- src/test/java/io/baxter/authentication/tests/api/controllers/ControllerTest.java
+++ src/test/java/io/baxter/authentication/tests/api/controllers/ControllerTest.java
@@ -1,12 +1,7 @@
 package io.baxter.authentication.tests.api.controllers;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.reactive.*;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.reactive.*;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.core.annotation.AliasFor;
 
@@ -17,14 +12,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @WebFluxTest
-@AutoConfigureWebTestClient
-@ImportAutoConfiguration(exclude = {
-        R2dbcAutoConfiguration.class,
-        R2dbcDataAutoConfiguration.class,
-        SecurityAutoConfiguration.class,
-        ReactiveSecurityAutoConfiguration.class,
-        ReactiveUserDetailsServiceAutoConfiguration.class
-})
 public @interface ControllerTest {
     @AliasFor(annotation = WebFluxTest.class, attribute = "controllers")
     Class<?>[] controllers() default {};
