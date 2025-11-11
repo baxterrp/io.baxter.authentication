f33d23256c5ba43a0c276c8b2dbdb2fa02b9c648
robert baxter <baxterrp@gmail.com>
2025-11-11 07:42:26 -0500

    removed unused test config class

--- src/test/java/io/baxter/authentication/tests/TestBootConfig.java
+++
@@ -1,7 +0,0 @@
-package io.baxter.authentication.tests;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "io.baxter.authentication.api")
-public class TestBootConfig {
-}
